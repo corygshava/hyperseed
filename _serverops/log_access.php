4e8ef3034e633e0c299c8c353fa58d7df0266bd7
corygshava <corygshava@gmail.com>
2025-10-23 10:23:16 +0300

    quick fix - visits

--- _serverops/log_access.php
+++ _serverops/log_access.php
@@ -1,15 +1,17 @@
 <?php
     require_once __DIR__.'/functions.php';
 
-    $res = '';
+    $res = '??';
+    $in = file_get_contents('php://input');
+    $input = $in == '' ? [] : json_decode($in,true);
 
-    if(isset($_GET['res']) || isset($_POST['res'])){
-        $res = $_GET['res'] ?? $_POST['res'];
+    if(isset($_GET['res']) || isset($_POST['res']) || isset($input['res'])){
+        $res = $_GET['res'] ?? ($_POST['res'] ?? $input['res']);
     }
 
     $timest = date('d-m-y h:i:s');
-    $resource = $res == '' ? 'unknown' : $res;
-    $logstring = "[{$timest}] -> user accessed $resource";
+    $resource = $res;
+    $logstring = "[{$timest}] -> user accessed: $resource";
     $path = __DIR__.'/../_datazone/visits.json';
 
     $rss = update_json($path,$logstring);
